f216882dcdfb6cc1d3d66b4b6bf6f147cdd89fa5
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-04-15 18:18:10 -0500

    add pokeball and fetch images unfinished function

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -2,10 +2,34 @@ var startButton = document.getElementById('startButton')
 var landingTitle = document.getElementById("landing-title")
 var redirectUrl = '.'
 var countDown = 24
-startButton.addEventListener('click', function() {
+var pokemonApiPics = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+var pokemonScrollDisplay = document.getElementById('scroll');
+var imgIndex = [];
+
+startButton.addEventListener('click', function () {
     location.assign('./index.html')
 })
 
-setInterval(function(){
+setInterval(function () {
     landingTitle.setAttribute("class", 'movement')
 }, 100)
+
+
+// Function to call pokemon images and display in a scrolling banner
+// Not done yet.....
+function scrollPokemon() {
+    fetch(pokemonApiPics)
+        .then(function (response) { 
+            return response.json() })
+        .then(function (data) {
+            for (var i = 0; i < data.results.length; i++) {
+                var pokeIMG = document.createElement('img');
+                pokeIMG.src = data.results[i].sprites.front_default;
+                pokemonScrollDisplay.appendChild(pokeIMG);
+              }
+            })
+            .catch(function(error) {
+              console.log('Error fetching Pokemon data:', error);
+            });
+        }
+scrollPokemon();

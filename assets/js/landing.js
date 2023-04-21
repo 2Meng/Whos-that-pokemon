8b9eae0f8a15ddf47d28ff9debad0118ad424924
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-04-20 20:21:08 -0500

    ok

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,128 +1,150 @@
-var startButton = document.getElementById('startButton')
-var landingTitle = document.getElementById("landing-title")
-var difficultyIMG = document.getElementById("pokeball")
-var difficultyDisplay = document.getElementById('difficulty-display')
+var startButton = document.getElementById("startButton");
+var landingTitle = document.getElementById("landing-title");
+var difficultyIMG = document.getElementById("pokeball");
+var difficultyDisplay = document.getElementById("difficulty-display");
 
 // Modal Stuff //
-var modalButton = document.getElementById('modal-btn')
-var pokeModal = document.getElementById('poke-modal')
-var closeSpan = document.getElementsByClassName('close')[0]
+var modalButton = document.getElementById("modal-btn");
+var pokeModal = document.getElementById("modal");
+var closeSpan = document.getElementsByClassName("close")[0];
 
-var nextButton = document.getElementById("next-button")
-var prevButton = document.getElementById("prev-button")
-var redirectUrl = '.'
-var pokemonApi = 'https://pokeapi.co/api/v2/pokemon/';
-var pokemonScrollDisplay = document.getElementById('scroll');
+var nextButton = document.getElementById("next-button");
+var prevButton = document.getElementById("prev-button");
+var redirectUrl = ".";
+var pokemonApi = "https://pokeapi.co/api/v2/pokemon/";
+var pokemonScrollDisplay = document.getElementById("scroll");
 
-var difficultyValue
-var imgArray = []
+var difficultyValue;
+var imgArray = [];
 
-modalButton.addEventListener('click', function() {
-    pokeModal.style.display = 'block'
-})
+modalButton.addEventListener("click", function () {
+  pokeModal.style.display = "block";
+});
 
-closeSpan.addEventListener('click', function() {
-    pokeModal.style.display = 'none'
-})
+closeSpan.addEventListener("click", function () {
+  pokeModal.style.display = "none";
+});
 
-nextButton.addEventListener('click', function() {
-    if(difficultyValue<imgArray.length-1){   
-        difficultyValue++
-    } else {
-        difficultyValue = 0
-    }
-    difficultyIMG.setAttribute('src', imgArray[difficultyValue])
-    if(difficultyValue == 0){
-        difficultyDisplay.innerHTML = "Easy Mode: Generation One Only"
-    }
-    if(difficultyValue == 1){
-        difficultyDisplay.innerHTML = "Great Mode: Generations One-Three"
-    }
-    if(difficultyValue == 2){
-        difficultyDisplay.innerHTML = "Ultra Mode: Generations One-Five"
-    }
-    if(difficultyValue == 3){
-        difficultyDisplay.innerHTML = "Master Mode: All Pokemon"
-    }
-})
+nextButton.addEventListener("click", function () {
+  if (difficultyValue < imgArray.length - 1) {
+    difficultyValue++;
+  } else {
+    difficultyValue = 0;
+  }
+  difficultyIMG.setAttribute("src", imgArray[difficultyValue]);
+  if (difficultyValue == 0) {
+    difficultyDisplay.innerHTML = "Easy Mode: Generation One Only";
+  }
+  if (difficultyValue == 1) {
+    difficultyDisplay.innerHTML = "Great Mode: Generations One-Three";
+  }
+  if (difficultyValue == 2) {
+    difficultyDisplay.innerHTML = "Ultra Mode: Generations One-Five";
+  }
+  if (difficultyValue == 3) {
+    difficultyDisplay.innerHTML = "Master Mode: All Pokemon";
+  }
+});
 
-prevButton.addEventListener('click', function() {
-    if(difficultyValue > 0){
-        difficultyValue--
-    } else {
-        difficultyValue = imgArray.length-1
-    }
-    difficultyIMG.setAttribute('src', imgArray[difficultyValue])
-    if(difficultyValue == 0){
-        difficultyDisplay.innerHTML = "Easy Mode: Generation One Only"
-    }
-    if(difficultyValue == 1){
-        difficultyDisplay.innerHTML = "Great Mode: Generations One-Three"
-    }
-    if(difficultyValue == 2){
-        difficultyDisplay.innerHTML = "Ultra Mode: Generations One-Five"
-    }
-    if(difficultyValue == 3){
-        difficultyDisplay.innerHTML = "Master Mode: All Pokemon"
-    }
-})
+prevButton.addEventListener("click", function () {
+  if (difficultyValue > 0) {
+    difficultyValue--;
+  } else {
+    difficultyValue = imgArray.length - 1;
+  }
+  difficultyIMG.setAttribute("src", imgArray[difficultyValue]);
+  if (difficultyValue == 0) {
+    difficultyDisplay.innerHTML = "Easy Mode: Generation One Only";
+  }
+  if (difficultyValue == 1) {
+    difficultyDisplay.innerHTML = "Great Mode: Generations One-Three";
+  }
+  if (difficultyValue == 2) {
+    difficultyDisplay.innerHTML = "Ultra Mode: Generations One-Five";
+  }
+  if (difficultyValue == 3) {
+    difficultyDisplay.innerHTML = "Master Mode: All Pokemon";
+  }
+});
 
-startButton.addEventListener('click', function () {
-    localStorage.setItem("difficulty", difficultyValue)
-    location.assign('./index.html')
-})
+startButton.addEventListener("click", function () {
+  localStorage.setItem("difficulty", difficultyValue);
+  location.assign("./game.html");
+});
 
 setInterval(function () {
-    landingTitle.setAttribute("class", 'movement')
-}, 1000)
+  landingTitle.setAttribute("class", "movement");
+}, 1000);
 
 function init() {
-    difficultyValue = 0
-    // load images to imgArray
-    imgArray[0] = './assets/photos/pokeball-16848.png'
-    imgArray[1] = './assets/photos/GreatBall.png'
-    imgArray[2] = './assets/photos/UltraBall.png'
-    imgArray[3] = './assets/photos/MasterBall.png'
-    difficultyIMG.setAttribute('src', imgArray[0])
-    difficultyDisplay.innerHTML = "Easy Mode: Generation One Only"
+  difficultyValue = 0;
+  // load images to imgArray
+  imgArray[0] = "./assets/photos/pokeball-16848.png";
+  imgArray[1] = "./assets/photos/GreatBall.png";
+  imgArray[2] = "./assets/photos/UltraBall.png";
+  imgArray[3] = "./assets/photos/MasterBall.png";
+  difficultyIMG.setAttribute("src", imgArray[0]);
+  difficultyDisplay.innerHTML = "Easy Mode: Generation One Only";
 }
 
-// Function to call pokemon images and display in a scrolling banner. 
+// Function to call pokemon images and display in a scrolling banner.
 function fetchPokemon() {
-    for (let i = 1; i < 151; i++) {
-        var pokemonApiPics = pokemonApi + i;
-        fetch(pokemonApiPics)
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
-                var pokeImg = document.createElement('img')
-                pokeImg.src = data.sprites.front_default
-                pokemonScrollDisplay.appendChild(pokeImg)
-            })
-    }
-} 
+  for (let i = 1; i < 151; i++) {
+    var pokemonApiPics = pokemonApi + i;
+    fetch(pokemonApiPics)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var pokeImg = document.createElement("img");
+        pokeImg.src = data.sprites.front_default;
+        pokemonScrollDisplay.appendChild(pokeImg);
+      });
+  }
+}
 
-window.addEventListener('load', function () {
-	function go() {
-		i = i < width ? i + step : 1;
-		m.style.marginLeft = -i + 'px';
-	}
-	var i = 0,
-		step = 3,
-		space = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	var m = document.getElementById('scroll');
-	var t = m.innerHTML;
-	m.innerHTML = t + space;
-	m.style.position = 'absolute';
-	var width = (m.clientWidth + 1);
-	m.style.position = '';
-	m.innerHTML = t + space + t + space + t + space + t + space + t + space + t + space + t + space;
-	m.addEventListener('mouseleave', function () {
-		step = 3;
-	}, true);
-	var x = setInterval(go, 50);
-}, true);
+window.addEventListener(
+  "load",
+  function () {
+    function go() {
+      i = i < width ? i + step : 1;
+      m.style.marginLeft = -i + "px";
+    }
+    var i = 0,
+      step = 3,
+      space = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    var m = document.getElementById("scroll");
+    var t = m.innerHTML;
+    m.innerHTML = t + space;
+    m.style.position = "absolute";
+    var width = m.clientWidth + 1;
+    m.style.position = "";
+    m.innerHTML =
+      t +
+      space +
+      t +
+      space +
+      t +
+      space +
+      t +
+      space +
+      t +
+      space +
+      t +
+      space +
+      t +
+      space;
+    m.addEventListener(
+      "mouseleave",
+      function () {
+        step = 3;
+      },
+      true
+    );
+    var x = setInterval(go, 50);
+  },
+  true
+);
 
 init();
 fetchPokemon();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,36 @@
-// DadJokes API // 
-var dadJokesAPI = 'https://icanhazdadjoke.com/';
-var generatedDadJoke = document.getElementById('dad-joke');
+// DadJokes API //
+var dadJokesAPI = "https://icanhazdadjoke.com/";
+var generatedDadJoke = document.getElementById("dad-joke");
+
+//Modal var//
+var jokes = document.getElementById("jokes");
+var pokeModal = document.getElementById("modal");
+var closeSpan = document.getElementsByClassName("close")[0];
 
 // Pokemon API //
-var pokemonApi = 'https://pokeapi.co/api/v2/pokemon/';
-var pokemonName = '';
+var pokemonApi = "https://pokeapi.co/api/v2/pokemon/";
+var pokemonName = "";
 
-// In-game UI // 
-var ingameUI = document.getElementById('ingame-ui');
-var actualGame = document.getElementById('game');
-var userHP = document.getElementById('user-health-points');
-var healthText = document.getElementById('health-text');
-var blankWordSpace = document.getElementById('letters-blanks');
-var incorrectLettersEl = document.getElementById('incorrect-letters')
-var playerMessage = document.getElementById('player-message');
-var countdown = document.getElementById('countdown');
-var randomPokemon = document.getElementById('guess-the-pokemon');
-var userScore = document.getElementById('user-score');
+// In-game UI //
+var ingameUI = document.getElementById("ingame-ui");
+var actualGame = document.getElementById("game");
+var userHP = document.getElementById("user-health-points");
+var healthText = document.getElementById("health-text");
+var blankWordSpace = document.getElementById("letters-blanks");
+var incorrectLettersEl = document.getElementById("incorrect-letters");
+var playerMessage = document.getElementById("player-message");
+var countdown = document.getElementById("countdown");
+var randomPokemon = document.getElementById("guess-the-pokemon");
+var userScore = document.getElementById("user-score");
 
 // Pokemon HINT/STATS //
-var displayHintBtn = document.getElementById('display-hint');
-var hintMessage = document.getElementById('hint');
-var pokemonStats = document.getElementById('pokemon-stats');
-var pokemonID = document.getElementById('pokemon-id');
-var pokemonHeight = document.getElementById('height');
-var pokemonWeight = document.getElementById('weight');
-var pokemonType = document.getElementById('type');
+var displayHintBtn = document.getElementById("display-hint");
+var hintMessage = document.getElementById("hint");
+var pokemonStats = document.getElementById("pokemon-stats");
+var pokemonID = document.getElementById("pokemon-id");
+var pokemonHeight = document.getElementById("height");
+var pokemonWeight = document.getElementById("weight");
+var pokemonType = document.getElementById("type");
 
 // Variables for the game //
 var gameBlanks = "";
@@ -40,37 +45,37 @@ var isLoadingPokemon = false;
 var pokemonHasBeenGuessed = false;
 var scoreArray = [];
 
-// Blanks for the user to guess Pokemon // 
+// Blanks for the user to guess Pokemon //
 var blankLetters = [];
 var guessPokemon = [];
 var incorrectLetters = [];
 
 // Variables from Local Storage for difficulty //
-var difficulty = localStorage.getItem("difficulty")
+var difficulty = localStorage.getItem("difficulty");
 var selectedDifficulty;
-if (difficulty == 0){
-    healthPoints = 100;
-    // GEN 1 //
-    selectedDifficulty = 151;
-} else if (difficulty == 1){
-    healthPoints = 75;
-    // GEN 1 - 3 //
-    selectedDifficulty = 386;
-} else if (difficulty == 2){
-    healthPoints = 50;
-    // GEN 1 - 5 //
-    selectedDifficulty = 649
-} else if (difficulty == 3){
-    healthPoints = 25;
-    // GEN 1 - 8 //
-    selectedDifficulty = 905
+if (difficulty == 0) {
+  healthPoints = 100;
+  // GEN 1 //
+  selectedDifficulty = 151;
+} else if (difficulty == 1) {
+  healthPoints = 75;
+  // GEN 1 - 3 //
+  selectedDifficulty = 386;
+} else if (difficulty == 2) {
+  healthPoints = 50;
+  // GEN 1 - 5 //
+  selectedDifficulty = 649;
+} else if (difficulty == 3) {
+  healthPoints = 25;
+  // GEN 1 - 8 //
+  selectedDifficulty = 905;
 }
 
 // Starts game //
-function initGame (){
-    loadPokemon()
-    handleUserHP()
-    clockTimer ()
+function initGame() {
+  loadPokemon();
+  handleUserHP();
+  clockTimer();
 }
 
 // Returns a random number/Pokemon within the number scope set //
@@ -81,43 +86,42 @@ function randomNumGen(x) {
 // Load Data from Pokemon API. Get Sprite, Get type? Get Evolution?
 // currently creates an Image Element and attaches it to an element in the document name tempId
 function loadPokemon() {
-    var pokemonGeneration = randomNumGen(selectedDifficulty)
-    randomPokemon.innerHTML = '';
-    isLoadingPokemon = true;
-    fetch(pokemonApi + pokemonGeneration)
-
+  var pokemonGeneration = randomNumGen(selectedDifficulty);
+  randomPokemon.innerHTML = "";
+  isLoadingPokemon = true;
+  fetch(pokemonApi + pokemonGeneration)
     .then(function (response) {
       return response.json();
     })
 
-        .then(function(data){
-        pokemonName = data.name
-        var pokeIMG = document.createElement('img')
-        pokeIMG.src = data.sprites.front_default
-        // diplay stats
-        pokemonID = document.createElement("li")
-        pokemonHeight = document.createElement("li")
-        pokemonWeight = document.createElement("li")
-        pokemonType = document.createElement("li")
-        pokemonStats.textContent = "POKÉMON STATS"
-        pokemonID.textContent = "ID: " + data.id
-        pokemonHeight.textContent = "HEIGHT: " + data.height
-        pokemonWeight.textContent = "WEIGHT: " + data.weight
-        pokemonType.textContent = "TYPE: " + data.types[0].type.name
-        pokemonStats.append(pokemonID, pokemonHeight, pokemonWeight, pokemonType)
+    .then(function (data) {
+      pokemonName = data.name;
+      var pokeIMG = document.createElement("img");
+      pokeIMG.src = data.sprites.front_default;
+      // diplay stats
+      pokemonID = document.createElement("li");
+      pokemonHeight = document.createElement("li");
+      pokemonWeight = document.createElement("li");
+      pokemonType = document.createElement("li");
+      pokemonStats.textContent = "POKÉMON STATS";
+      pokemonID.textContent = "ID: " + data.id;
+      pokemonHeight.textContent = "HEIGHT: " + data.height;
+      pokemonWeight.textContent = "WEIGHT: " + data.weight;
+      pokemonType.textContent = "TYPE: " + data.types[0].type.name;
+      pokemonStats.append(pokemonID, pokemonHeight, pokemonWeight, pokemonType);
 
-        // to set color to black and white
-        // pokeIMG.setAttribute('class', 'constrast-200, brightness-0)
-        randomPokemon.appendChild(pokeIMG)
-        isLoadingPokemon = false;
-        
-        console.log(data)
-        console.log(pokemonName)
-        renderBlanks(pokemonName)
-        })
-        .catch(function(error){
-        console.log('Error fetching Pokemon data:', error);
-        isLoadingPokemon = false;
+      // to set color to black and white
+      // pokeIMG.setAttribute('class', 'constrast-200, brightness-0)
+      randomPokemon.appendChild(pokeIMG);
+      isLoadingPokemon = false;
+
+      console.log(data);
+      console.log(pokemonName);
+      renderBlanks(pokemonName);
+    })
+    .catch(function (error) {
+      console.log("Error fetching Pokemon data:", error);
+      isLoadingPokemon = false;
     });
 }
 
@@ -151,110 +155,112 @@ function checksLetters(letters) {
     }
     blankWordSpace.textContent = blankLetters.join(" ");
 
-        if(blankLetters.join('') === gameBlanks){
-            playerMessage.textContent = 'Congrats! Its ' + pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1) + '!';
-            scoreCounter ++;
-            timerCount += 5;
-            pokemonHasBeenGuessed = true;
-            setTimeout(nextPokemon, 2000);
-        }
-
-    } 
-    if (!letterInWord) {
-        guessPokemon.push(letters);
-        incorrectLetters.push(letters);
-        playerMessage.textContent = 'Oops! Looks like that letter isnt in the Pokemons name! Try Again!';
-        incorrectLettersEl.textContent = incorrectLetters.join(', ');
+    if (blankLetters.join("") === gameBlanks) {
+      playerMessage.textContent =
+        "Congrats! Its " +
+        pokemonName.charAt(0).toUpperCase() +
+        pokemonName.slice(1) +
+        "!";
+      scoreCounter++;
+      timerCount += 5;
+      pokemonHasBeenGuessed = true;
+      setTimeout(nextPokemon, 2000);
+    }
+  }
+  if (!letterInWord) {
+    guessPokemon.push(letters);
+    incorrectLetters.push(letters);
+    playerMessage.textContent =
+      "Oops! Looks like that letter isnt in the Pokemons name! Try Again!";
+    incorrectLettersEl.textContent = incorrectLetters.join(", ");
 
     setTimeout(function () {
       playerMessage.textContent = "";
     }, 1250);
 
-        if (guessPokemon.length === 10) {
-            playerMessage.textContent = 'Oh no! The Pokemons name was.. ' + pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1) + '! Try again!';
-            scoreCounter --;
-            timerCount -= 5;
-            setTimeout(nextPokemon, 2000);
-        }
-        // Counts the number of incorrect characters guessed //
-        incorrectKeyGuesses = incorrectLetters.length;
+    if (guessPokemon.length === 10) {
+      playerMessage.textContent =
+        "Oh no! The Pokemons name was.. " +
+        pokemonName.charAt(0).toUpperCase() +
+        pokemonName.slice(1) +
+        "! Try again!";
+      scoreCounter--;
+      timerCount -= 5;
+      setTimeout(nextPokemon, 2000);
     }
-};
+    // Counts the number of incorrect characters guessed //
+    incorrectKeyGuesses = incorrectLetters.length;
+  }
+}
 
 // Event listener for keys presses //
-document.addEventListener('keyup', function(event){
-    // Converts the keys pressed to lower case letters //
-    var key = event.key.toLowerCase();
-    var availableCharacters = 'abcdefghijklmnopqrstuvwxyz123456789-'.split('');
-    
-    // If statement for letters pushed //
+document.addEventListener("keyup", function (event) {
+  // Converts the keys pressed to lower case letters //
+  var key = event.key.toLowerCase();
+  var availableCharacters = "abcdefghijklmnopqrstuvwxyz123456789-".split("");
 
-    if(availableCharacters.includes(key)){
-        if(pokemonHasBeenGuessed){
-            return;
-        }
-        var pressedLetter = event.key;
-        checksLetters(pressedLetter)
+  // If statement for letters pushed //
+
+  if (availableCharacters.includes(key)) {
+    if (pokemonHasBeenGuessed) {
+      return;
     }
+    var pressedLetter = event.key;
+    checksLetters(pressedLetter);
+  }
 });
 
-function nextPokemon(){
-    // Resets game elements for next Pokemon // 
-    randomPokemon.innerHTML = '';
-    blankWordSpace.textContent = '';
-    playerMessage.textContent = '';
-    guessPokemon = [];
-    incorrectLettersEl.textContent = '';
-    incorrectLetters = [];
-    pokemonHasBeenGuessed = false;
-    loadPokemon()
-    handleUserHP()
+function nextPokemon() {
+  // Resets game elements for next Pokemon //
+  randomPokemon.innerHTML = "";
+  blankWordSpace.textContent = "";
+  playerMessage.textContent = "";
+  guessPokemon = [];
+  incorrectLettersEl.textContent = "";
+  incorrectLetters = [];
+  pokemonHasBeenGuessed = false;
+  loadPokemon();
+  handleUserHP();
 }
 
-function handleUserHP(){
-    // Subtracts points from user HP per incorrect key guesses //
-    healthPoints -= incorrectKeyGuesses
-    var maxHealth = 100;
-    var healthPercentage = Math.max(0, healthPoints) / maxHealth
-    healthText.textContent = 'Player HP: ' + healthPoints + 'HP'
-    userHP.style.width = (healthPercentage * 100) + '%';
+function handleUserHP() {
+  // Subtracts points from user HP per incorrect key guesses //
+  healthPoints -= incorrectKeyGuesses;
+  var maxHealth = 100;
+  var healthPercentage = Math.max(0, healthPoints) / maxHealth;
+  healthText.textContent = "Player HP: " + healthPoints + "HP";
+  userHP.style.width = healthPercentage * 100 + "%";
 
-    // Should reset guesses //
-    incorrectKeyGuesses = 0
+  // Should reset guesses //
+  incorrectKeyGuesses = 0;
 
-    if (healthPoints === 0 || healthPoints < 0){
-        endGame()
+  if (healthPoints === 0 || healthPoints < 0) {
+    endGame();
+  }
+}
+
+function clockTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    countdown.textContent = "Time left: " + timerCount;
+
+    if (timerCount === 0) {
+      clearInterval(timer);
+      endGame();
     }
+  }, 1000);
 }
 
-function clockTimer (){
-    timer = setInterval(function(){
-        timerCount --
-        countdown.textContent = 'Time left: ' + timerCount;
+displayHintBtn.addEventListener("click", function () {});
 
-        if(timerCount === 0) {
-            clearInterval(timer);
-            endGame()
-        }
-    }, 1000);
-}
+closeSpan.addEventListener("click", function () {
+  pokeModal.style.display = "none";
+});
 
-displayHintBtn.addEventListener('click', function (){
-
-})
-
-function endGame (){
-    ingameUI.textContent = 'GAME OVER!';
-    userScore.textContent = 'Your score!: ' + scoreCounter;
-
-    scoreArray.push(scoreCounter)
-    localStorage.setItem('endscore', scoreArray);
-}
-
-initGame ()
+initGame();
 
 // Runs dad jokes API
-function randomDadJoke(dadJokesAPI) {
+function randomDadJoke() {
   fetch(dadJokesAPI, {
     headers: {
       Accept: "application/json",
@@ -264,11 +270,18 @@ function randomDadJoke(dadJokesAPI) {
       return response.json();
     })
     .then(function (data) {
-      generatedDadJoke.textContent = data.joke;
+      jokes.textContent = data.joke;
     });
 }
-
-
+function endGame() {
+  ingameUI.textContent = "GAME OVER!";
+  userScore.textContent = "Your score!: " + scoreCounter;
+  randomDadJoke();
+  scoreArray.push(scoreCounter);
+  localStorage.setItem("endscore", scoreArray);
+  pokeModal.style.display = "block";
+}
+console.log(jokes);
 
 // Open the modal
 function openModal() {
@@ -279,29 +292,3 @@ function openModal() {
 function closeModal() {
   modal.classList.remove("open");
 }
-
-
-
-// Open the modal and load API data
-function openModal() {
-  modal.style.display = "block"; // Make API request
-
-  fetch("https://icanhazdadjoke.com/")
-    .then((response) => response.json())
-    .then((data) => {
-      // Populate modal data container with API data
-      modalData.innerHTML = `https://icanhazdadjoke.com/`
-        <p>${data.title}</p>
-        <img src="${data.image}" alt="${data.title}">
-        <p>${data.description}</p>
-      `;
-    })
-    .catch((error) => console.error(error));
-}
-
-// Close the modal
-function closeModal() {
-  modal.style.display = "none";
-}
-
-
